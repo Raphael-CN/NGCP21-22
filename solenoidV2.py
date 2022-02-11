a9a166dbbc7f4f818d96a6d82c33ed15fd0fdfbd
Raphael-CN <89231654+Raphael-CN@users.noreply.github.com>
2022-02-11 10:42:42 -0800

    Add files via upload

---
+++ solenoidV2.py
@@ -0,0 +1,13 @@
+import pigpio
+
+pi = pigpio.pi()
+
+solenoidENA = 13 #pin number (BCM) for ENA input on driver
+solenoidIn1 = 6 #pin number (BCM) for In1 input on driver
+solenoidIn2 = 5 #pin number (BCM) for In2 input on driver
+
+pi.set_mode(solenoidENA, pigpio.OUTPUT)
+pi.set_mode(solenoidIn1, pigpio.OUTPUT)
+pi.set_mode(solenoidIn2, pigpio.OUTPUT)
+
+pi.set_PWM_dutycycle(solenoidENA, 50)
