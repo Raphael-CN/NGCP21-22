141c61f5b3649c1e1b1ccd9c582d11f8d3b82bd0
Raphael-CN <89231654+Raphael-CN@users.noreply.github.com>
2022-02-11 11:10:17 -0800

    Update solenoid.py

--- solenoid.py
+++ solenoid.py
@@ -5,13 +5,13 @@ GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BCM)
 # Initialize BCM pin 18 as output
-GPIO.setup(13, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 while True:
 
     #This Turns Relay Off. Brings Voltage to Max GPIO can output ~3.3V
-    GPIO.output(13, 1)
+    GPIO.output(18, 1)
     sleep(1)
     # Turns Relay On. Brings Voltage to Min GPIO can output ~0V.
-    GPIO.output(13, 0)
+    GPIO.output(18, 0)
     sleep(1)
