fc36ecec7177c69347b54f04ccc3b15cdb700602
Raphael-CN <89231654+Raphael-CN@users.noreply.github.com>
2022-02-12 10:38:04 -0800

    Update solenoid.py

--- solenoid.py
+++ solenoid.py
@@ -11,10 +11,10 @@ while True:
 
     #This Turns Relay Off. Brings Voltage to Max GPIO can output ~3.3V
     GPIO.output(18, 1)
-    sleep(10)
+    sleep(1000)
     # Turns Relay On. Brings Voltage to Min GPIO can output ~0V.
     GPIO.output(18, 0)
-    sleep(10)
+    sleep(1000)
     
     GPIO.output(18, 1)
-    sleep(10)
+    sleep(1000)
