e8f1d0f677018b74d50b957c900749f2c9e690f4
Raphael-CN <89231654+Raphael-CN@users.noreply.github.com>
2022-02-12 11:05:04 -0800

    Update solenoid.py

--- solenoid.py
+++ solenoid.py
@@ -6,7 +6,7 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 # Initialize BCM pin 18 as output
 GPIO.setup(18, GPIO.OUT)
-solenoid = GPIO.PWM(18, 0.5)
+solenoid = GPIO.PWM(18, 10)
 
 solenoid.start(100)
 input('Press return to stop:')   
