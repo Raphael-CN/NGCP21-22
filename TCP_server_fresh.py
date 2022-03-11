1d4655ba4805c03575a90e921ac16239e8a6d90a
Raphael-CN <89231654+Raphael-CN@users.noreply.github.com>
2022-03-11 12:38:38 -0800

    Update TCP_server_fresh.py

--- TCP_server_fresh.py
+++ TCP_server_fresh.py
@@ -4,27 +4,24 @@ import sys
 from struct import *
 import base64
 
-TCP_IP = "192.168.1.95" # listen to everything
+TCP_IP = "192.168.86.248" # listen to everything
 TCP_PORT = 12345 # port
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server_socket.bind((TCP_IP, TCP_PORT))
-server_socket.listen()
 
 # Define variables
 axis = np.zeros(6)
 button = np.zeros(16)
 
-connection, addr = server_socket.accept()
-# data, addr = server_socket.recv(512) # random buffer size, doesn't matter here..
+data, addr = server_socket.recvfrom(512) # random buffer size, doesn't matter here..
 print("Beginning client communication: ")
 
-# server_socket.sendall(b'Server communication established')
-with connection:
-    while True:
-        for i in range(6):
-            axis[i], addr = connection.recv(512)
-        for i in range(16):
-            button[i], addr = connection.recv(512)
-        print(axis)
-        print(button)
+server_socket.sendto(b'Server communication established', addr)
+while True:
+    for i in range(6):
+        axis[i], addr = server_socket.recvfrom(512)
+    for i in range(16):
+        button[i], addr = server_socket.recvfrom(512)
+    print(axis)
+    print(button)
