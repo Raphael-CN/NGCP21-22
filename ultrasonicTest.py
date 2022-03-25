fcba6f196cc04c4ce45f526d83ced5c5ff9364e4
Raphael-CN <89231654+Raphael-CN@users.noreply.github.com>
2022-03-25 14:24:42 -0700

    Update ultrasonicTest.py

--- ultrasonicTest.py
+++ ultrasonicTest.py
@@ -4,50 +4,31 @@ import time
 try:
     GPIO.setmode(GPIO.BOARD)
 
-    PIN_TRIGGER_1 = 7
-    PIN_ECHO_1 = 11
+    PIN_TRIGGER = 7
+    PIN_ECHO = 11
 
-    PIN_TRIGGER_2 = 29
-    PIN_ECHO_2 = 31
+    GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+    GPIO.setup(PIN_ECHO, GPIO.IN)
 
-    GPIO.setup(PIN_TRIGGER_1, GPIO.OUT)
-    GPIO.setup(PIN_ECHO_1, GPIO.IN)
-
-    GPIO.setup(PIN_TRIGGER_2, GPIO.OUT)
-    GPIO.setup(PIN_ECHO_2, GPIO.IN)
-
-    GPIO.output(PIN_TRIGGER_1, GPIO.LOW)
-    GPIO.output(PIN_TRIGGER_2, GPIO.LOW)
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
     while True:
 
-        GPIO.output(PIN_TRIGGER_1, GPIO.HIGH)
-        GPIO.output(PIN_TRIGGER_2, GPIO.HIGH)
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
 
         time.sleep(0.00001)
 
-        GPIO.output(PIN_TRIGGER_1, GPIO.LOW)
-        GPIO.output(PIN_TRIGGER_2, GPIO.LOW)
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-        while GPIO.input(PIN_ECHO_1)==0:
-            pulse_start_time_1 = time.time()
-        while GPIO.input(PIN_ECHO_1)==1:
-            pulse_end_time_1 = time.time()
+        while GPIO.input(PIN_ECHO)==0:
+            pulse_start_time = time.time()
+        while GPIO.input(PIN_ECHO)==1:
+            pulse_end_time = time.time()
 
-        while GPIO.input(PIN_ECHO_2)==0:
-            pulse_start_time_2 = time.time()
-        while GPIO.input(PIN_ECHO_2)==1:
-            pulse_end_time_2 = time.time()
-
-        pulse_duration_1 = pulse_end_time_1 - pulse_start_time_1
-        distance = round(pulse_duration_1 * 17150, 2)
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = round(pulse_duration * 17150, 2)
         inches = round(distance * .393701, 2)
         print ("Distance:",distance,"cm ", "Inches:", inches)
-
-        pulse_duration_2 = pulse_end_time_2 - pulse_start_time_2
-        distance2 = round(pulse_duration_2 * 17150, 2)
-        inches2 = round(distance2 * .393701, 2)
-        print ("Distance:",distance2,"cm ", "Inches:", inches2)
 
         time.sleep(.5)
 except KeyboardInterrupt:
